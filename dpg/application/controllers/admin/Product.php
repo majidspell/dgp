29f318a71a1d461b30f02d150ecb5df9e5dcd33b
majid <majid_spell2@yahoo.co.id>
2015-10-22 00:20:38 +0700

    edit product

--- dpg/application/controllers/admin/Product.php
+++ dpg/application/controllers/admin/Product.php
@@ -12,47 +12,46 @@ class Product extends CI_Controller {
         $this->template->load('dashboard', 'admin/product/data', $data);
     }
 
+    function validation_product() {
+        $this->form_validation->set_rules('nama_product', 'Nama_product', 'trim|required');
+        $this->form_validation->set_rules('harga', 'Harga', 'trim|required|numeric');
+        $this->form_validation->set_rules('kategori', 'Kategori', 'required');
+        $config = array(
+            'upload_path' => './pictures/',
+            'allowed_types' => 'gif|jpg|png', // Jenis file yang di ijinkan
+            'file_name' => 'file_' . date('dmYHis'), // Codeigniter otomatis akan merename file
+            'file_ext_tolower' => TRUE, // mengubah extensi menjadi huruf kecil
+            'overwrite' => TRUE, // Jika bernilai TRUE maka file dengan nama yang sama akan ditimpa
+            'max_size' => 3000, // Maksimal ukuran file dalam Kilobyte, jika di isi 0 maka tidak terhingga
+            'max_width' => 3000, // maksimal panjang gambar dalam ukuran pixel
+            'max_height' => 3000, // maksimal lebar gambar dalam ukuran pixel  
+            'min_width' => 10, // minimal panjang gambar dalam ukuran pixel  
+            'min_height' => 7, // minimal lebar gambar dalam ukuran pixel      
+            'remove_spaces' => TRUE
+        );
+        $this->upload->initialize($config);
+    }
+
     function add() {
         $data['kategori'] = $this->kategori_model->select_all()->result();
         $this->template->load('dashboard', 'admin/product/add', $data);
     }
 
     function insert() {
-        $config = array(
-            'upload_path' => './pictures/', // Lokasi file yang di upload
-            'allowed_types' => 'gif|jpg|png', // Jenis file yang di ijinkan
-            'file_name' => 'file_' . date('dmYHis'), // Codeigniter otomatis akan merename file
-            'file_ext_tolower' => TRUE, // mengubah extensi menjadi huruf kecil
-            'overwrite' => TRUE, // Jika bernilai TRUE maka file dengan nama yang sama akan ditimpa
-            'max_size' => 1000, // Maksimal ukuran file dalam Kilobyte, jika di isi 0 maka tidak terhingga
-            'max_width' => 1850, // maksimal panjang gambar dalam ukuran pixel
-            'max_height' => 1850, // maksimal lebar gambar dalam ukuran pixel  
-            'min_width' => 10, // minimal panjang gambar dalam ukuran pixel  
-            'min_height' => 7, // minimal lebar gambar dalam ukuran pixel   
-            'max_filename' => 0,
-            'remove_spaces' => TRUE // Jika filename mengandung karakter spasi maka akan diganti dengan garisbawah (underline)
-        );
-        $this->load->library('upload', $config);
-////////////////////////////////////////////////////////////////////////
-        $this->form_validation->set_rules('nama_product', 'Nama_product', 'trim|required');
-        $this->form_validation->set_rules('harga', 'Harga', 'trim|required|numeric');
-        $this->form_validation->set_rules('kategori', 'Kategori', 'required');
-        if ($this->form_validation->run() == FALSE && !$this->upload->do_upload()) {
+        $this->validation_product();
+        if ($this->form_validation->run() == TRUE && $this->upload->do_upload("gambar")) {
+            $hasil = $this->upload->data(); //Menampilkan pesan sukses  
+            $this->product_model->insert($hasil);
+        } else {
             $hasil = $this->upload->display_errors(); //Menampilkan pesan error
             $data = array(
                 'correct' => 'salah',
                 'message_nama_product' => form_error('nama_product'),
                 'message_harga' => form_error('harga'),
                 'message_kategori' => form_error('kategori'),
-                'message_userfile' => $hasil
+                'message_gambar' => $hasil
             );
             echo json_encode($data);
-        } else {     
-                $hasil = $this->upload->data(); //Menampilkan pesan sukses  
-//            print_r($hasil);
-//            die();
-            $this->product_model->insert($hasil['file_name']);
-            /////////////////////////////////////////////////////////////////////////////////////////
         }
     }
 
@@ -63,13 +62,58 @@ class Product extends CI_Controller {
         $this->template->load('dashboard', 'admin/product/edit', $data);
     }
 
+    function update() {
+        $this->validation_product();
+        if ($this->form_validation->run() == TRUE && $this->upload->do_upload("gambar")) {
+            $hasil = $this->upload->data(); //Menampilkan pesan sukses  
+            $this->product_model->update($hasil);
+        } else {
+            $hasil = $this->upload->display_errors(); //Menampilkan pesan error
+            $data = array(
+                'correct' => 'salah',
+                'message_nama_product' => form_error('nama_product'),
+                'message_harga' => form_error('harga'),
+                'message_kategori' => form_error('kategori'),
+                'message_gambar' => $hasil
+            );
+            echo json_encode($data);
+        }
+    }
+
+    function upload() {
+        $config = array(
+            'upload_path' => './pictures/',
+            'allowed_types' => 'gif|jpg|png', // Jenis file yang di ijinkan
+            'file_name' => 'file_' . date('dmYHis'), // Codeigniter otomatis akan merename file
+            'file_ext_tolower' => TRUE, // mengubah extensi menjadi huruf kecil
+            'overwrite' => TRUE, // Jika bernilai TRUE maka file dengan nama yang sama akan ditimpa
+            'max_size' => 3000, // Maksimal ukuran file dalam Kilobyte, jika di isi 0 maka tidak terhingga
+            'max_width' => 3000, // maksimal panjang gambar dalam ukuran pixel
+            'max_height' => 3000, // maksimal lebar gambar dalam ukuran pixel  
+            'min_width' => 10, // minimal panjang gambar dalam ukuran pixel  
+            'min_height' => 7, // minimal lebar gambar dalam ukuran pixel      
+            'remove_spaces' => TRUE
+        );
+        $this->upload->initialize($config);
+        $id = $this->input->post('product_id');
+        if ($this->upload->do_upload("gambar")) {
+            $hasil = $this->upload->data(); //Menampilkan pesan sukses  
+            $this->product_model->upload($hasil, $id);
+        } else {
+            $hasil = $this->upload->display_errors(); //Menampilkan pesan error
+            echo $hasil;
+        }
+    }
+
+    function refresh() {
+        $gambarUpload = $this->input->post('gambarUpload');
+        echo "<img src='" . base_url() . "pictures/" . $gambarUpload . "' width='134px' height='134px' alt='no image found'/>";
+    }
+
     function delete() {
         $id = $_GET['product_id'];
         $this->product_model->delete($id);
     }
 
-    public function upload_file() {
-        
-    }
-
 }
+
